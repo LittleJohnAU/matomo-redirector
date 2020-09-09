1747e6dc0f7a43d5890083592e96b589a8e00667
Little John <70997252+LittleJohnAU@users.noreply.github.com>
2020-09-10 08:30:59 +1000

    Initial upload

---
+++ director.php
@@ -0,0 +1,97 @@
+<?php
+class Director
+{
+    protected static $chars = "abcdfghjkmnpqrstvwxyz|ABCDFGHJKLMNPQRSTVWXYZ|0123456789";
+    protected $pdo;
+    public $error;
+    // change these settings
+    protected $dbUsername = "db_username";
+    protected $dbPassword = "db_password";
+    protected $dbName     = "db_name";
+    public $matomoSiteId = 1;  // Site ID
+    public $matomoUrl = "https://matomo.mydomain.com"; // Your matomo URL
+    private $matomoToken = "";     // your api token
+    public $redirectDomain = "https://short.mydomain.com/";   // with trailing slash  
+    private $query = "SELECT `id`, `url` FROM matomo_shortcode WHERE code = :short_code LIMIT 1";
+
+    public function __construct(){
+        $this->pdo = new PDO("mysql:host=localhost;dbname=".$this->dbName, $this->dbUsername, $this->dbPassword);
+        $this->timestamp = date("Y-m-d H:i:s");
+    }
+    
+    public function setError($err){
+        $this->error = $err;
+    }
+
+    public function showHtml(){
+        $html = file_get_contents("template.html");
+        return str_replace("{{error}}",$this->error,$html);
+    }
+
+    public function getReferrer()
+    {
+        if (!empty($_SERVER['HTTP_REFERER'])) {
+            return $_SERVER['HTTP_REFERER'];
+        }
+        return false;
+    }
+
+    public function getToken(){
+        return $this->matomoToken;
+    }
+
+    public function removeParams($code){
+        if(stristr($code,'?')){
+            $sc = explode("?",$code);
+            $code = $sc[0];
+        }
+        return $code;
+    }
+    
+    public function shortToLong($code){
+        if(empty($code)) {
+            $this->error = "No short code was supplied";
+            throw new Exception("No short code was supplied");
+        }
+
+        if($this->validateShortCode($code) == false){
+            $this->error = "Short code is not valid";
+            throw new Exception("Short code is not valid");
+        }
+
+        $urlRow = $this->getUrlFromDB($code);
+        if(empty($urlRow)){
+            $this->error = "Short code does not appear to exist.";
+            throw new Exception("Short code does not appear to exist.");
+        }
+
+        return $urlRow["url"];
+    }
+
+    protected function validateShortCode($code){
+        $rawChars = str_replace('|', '', self::$chars);
+        return preg_match("|[".$rawChars."]+|", $code);
+    }
+
+    protected function getUrlFromDB($code){
+        $stmt = $this->pdo->prepare($this->query);
+        $params=array(
+            "short_code" => $code
+        );
+        $stmt->execute($params);
+
+        $result = $stmt->fetch();
+        return (empty($result)) ? false : $result;
+    }
+
+    public function closeDB(){
+        if($this->pdo){
+            $this->pdo = null;
+            return true;
+        }else{
+            $this->error = "Error: Connection to database lost.";
+            throw new Exception("Connection to database lost.");
+            return false;
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+ini_set('session.cookie_domain', '.mydomain.com'); // set this to your domain
+session_name("mydomain"); // this needs to be set on the target domain as well
+session_start();
+date_default_timezone_set('Australia/Brisbane'); // set this to your timezone or comment out if not needed
+
+require_once 'director.php';
+$dir = new Director();
+
+if(isset($_GET["c"])){
+    $shortCode = $dir->removeParams($_GET["c"]);
+    $url = $dir->shortToLong($shortCode);
+    
+    // Optional variable
+    $matomoPageTitle = "My Redirector"; // The title of the page
+    
+    // Load object
+    require_once("MatomoTracker.php");
+    $ref = $dir->getReferrer();
+    // Matomo object
+    $matomoTracker = new MatomoTracker((int)$dir->matomoSiteId, $dir->matomoUrl);
+    $token = $dir->getToken();
+    $matomoTracker->setTokenAuth($token);
+    $matomoTracker->setIdSite(1); // set this to the id for your target domain
+    $matomoTracker->setUrl($dir->redirectDomain.$shortCode);
+    $matomoTracker->setUrlReferrer($ref);
+    
+    $vid = $matomoTracker->getVisitorId();
+    $_SESSION['matreferrer'] = $ref; // see the readme about unsetting these at the target
+    $_SESSION['matvisitor'] = $vid; // this needs to be passed to the target domain
+    $dir->closeDB(); // close the database connection
+
+    try{
+        $matomoTracker->doTrackEvent('shortcode', 'redirect', $shortCode);
+        header("HTTP/1.1 301 Moved Permanently");
+        // Redirect to the original URL
+        header("Location: ".$url);
+        exit;
+    }catch(Exception $e){
+        unset($_SESSION['matvisitor']);
+        unset($_SESSION['matreferrer']);
+        // Display error
+        echo $dir->showHtml();
+    }
+} else {
+    $dir->setError("No short code was supplied.");
+    echo $dir->showHtml();
+}
